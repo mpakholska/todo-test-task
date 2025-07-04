d7033e050f05560b6699f27aec65530b9d1326cf
mpakholska <marta.pakholska.work@gmail.com>
2025-07-04 16:38:32 +0300

    create envs

--- apps/web/shared/utils/api/index.ts
+++ apps/web/shared/utils/api/index.ts
@@ -1,8 +1,20 @@
 import axios from 'axios';
 
 const $api = axios.create({
-  baseURL: 'http://localhost:3001',
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   withCredentials: true,
 });
+
+$api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      if (typeof window !== 'undefined') {
+        window.location.reload();
+      }
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default $api;
